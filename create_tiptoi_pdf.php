a16f5dd8077c92018a74f15f55e54d010cfc341b
Martin Helfer <martin.helfer@ub.uni-freiburg.de>
2019-04-15 21:53:41 +0200

    Unterscheidung bar-count split und normale mp3

--- create_tiptoi_pdf.php
+++ create_tiptoi_pdf.php
@@ -3,18 +3,22 @@
 use Mpdf\Mpdf;
 require_once __DIR__ . '/vendor/autoload.php';
 
-//Nach wie vielen Takten startet die neue Uebung (fuer Split der mp3)?
-$bar_count = 8;
+//Projektname (Name des Ordners in dem die mp3 files liegen)
+$project_name = "je-veux-str-v2";
 
-//Projektname
-$project_name = "pick-vol-1";
+//JSON-Config laden. Hier ist neben project-id und Ueberschrift auch hinterlegt, welche Files es gibt und wie sie optisch strukturiert sind
+$config = json_decode(file_get_contents(__DIR__ . "/config/" . $project_name . ".json"), true);
+$product_id = $config["product-id"];
+
+//Nach wie vielen Takten startet die neue Uebung (fuer Split der mp3)?
+$bar_count = $config["bar-count"];
 
 //In Projektordner wechseln
 $dir = "C:\Users\Martin\Desktop\Google Drive\Martin\TipToi Tool\david-maya";
 $project_dir = $dir . "/" . $project_name;
 chdir($project_dir);
 
-//Ueber mp3s gehen, die gesplittet werden muessen
+//Ueber mp3s gehen, die gesplittet werden muessen (diese Dateien gibt es nur bei Uebungen, die gesplittet werden muessen)
 foreach (glob("full_*.mp3") as $full_file) {
 
     //full_080 -> 3-stelliges Tempo extrahieren
@@ -28,13 +32,18 @@ foreach (glob("full_*.mp3") as $full_file) {
     $out = shell_exec('ffmpeg -hide_banner -loglevel panic -i ' . $full_file . ' -f segment -segment_time ' . $split_time . ' -c copy split_t_%0d_' . $tempo . '.mp3');
 }
 
-//JSON-Config laden. Hier ist neben project-id und Ueberschrift auch hinterlegt, welche Files es gibt und wie sie optisch strukturiert sind
-$config = json_decode(file_get_contents(__DIR__ . "/config/" . $project_name . ".json"), true);
-$product_id = $config["product-id"];
+//HTML fuer PDF-Datei mit Codes erstellen: Ueberschrift oben
+$html = "<h1>" . $config["header"] . "</h1>";
+
+//Anmelde-Symbol
+$html .= "<img src='oid-" . $product_id . "-START.png' />";
+
+//Stop-Symbol
+$html .= "<div><h2>Stop</h2><img src='oid-" . $product_id . "-stop.png' /></div>";
 
 //Yaml-Datei erzeugen
 $yaml_file = $project_name . ".yaml";
-$fh = fopen($project_dir . "/" . $yaml_file, "w");
+$fh = fopen($yaml_file, "w");
 
 //Product-ID eintragen
 fwrite($fh, "product-id: " . $product_id . "\n\n");
@@ -55,14 +64,43 @@ foreach (glob("../*.mp3") as $file) {
 
 //Ueber Rows (=Uebungen) und deren Files (=Geschwindigkeiten) der config gehen
 foreach ($config["rows"] as $row) {
+
+    //Ueberschrift der Uebung ("Uebung 1" vs. "Rechte Hand")
+    $html .= "<div><h2>" . $row["label"] . "</h2>";
+
+    //Benennung und OID-Code der Uebung als th und td einer Tabelle
+    $th_row = "";
+    $td_row = "";
+
+    //Ueber files (=Tempos) einer Uebung gehen
     foreach ($row["files"] as $file) {
+
+        //Name (=Tempo 80)
+        $th_row .= "<th>" . $file["name"] . "</th>";
+
+        //OID-Code als Bild
+        $td_row .= "<td><img src='oid-" . $product_id . "-t_" . $row["id"] . "_" . $file["id"] . ".png' /></td>";
 
         //Abspielcode in YAML-Datei als Script hinterlegen
         fwrite($fh, "  t_" . $row["id"] . "_" . $file["id"] . ": P(t_" . $row["id"] . "_" . $file["id"] . ")\n");
 
-        //Count-in-Datei und Split-Datei einer Uebung zu einer mp3 zusammenfuehren
-        shell_exec('copy /b count_in_' . $file["id"] . '.mp3+split_t_' . ($row["id"] - 1) . "_" . $file["id"] . '.mp3 t_' . $row["id"] . '_' . $file["id"] . '.mp3');
+        //Wenn die Uebung als gesplittete Datei vorliegt (z.B. pick a pick Uebung 1 als split der full-Datei Uebung 1-4)
+        if ($bar_count > 0) {
+
+            //Count-in-Datei und Split-Datei einer Uebung zu einer mp3 zusammenfuehren
+            shell_exec('copy /b count_in_' . $file["id"] . '.mp3+split_t_' . ($row["id"] - 1) . "_" . $file["id"] . '.mp3 t_' . $row["id"] . '_' . $file["id"] . '.mp3');
+        }
+
+        //Datei liegt bereits als vollstaendige Datei vor (z.B. je veux -> rechte Hand)
+        else {
+
+            //Count-in-Datei und  voll staendige Datei zu einer mp3 zusammenfuehren
+            shell_exec('copy /b count_in_' . $file["id"] . '.mp3+' . $row["id"] . "_" . $file["id"] . '.mp3 t_' . $row["id"] . '_' . $file["id"] . '.mp3');
+        }
     }
+
+    //fuer diese Uebung eine Tabelle anlegen (Ueberschriften als th + Bilder als td)
+    $html .= "<table><tr>" . $th_row . "</tr><tr>" . $td_row . "</tr></table></div>";
 }
 fclose($fh);
 
@@ -76,39 +114,6 @@ shell_exec('tttool oid-codes ' . $yaml_file . ' --pixel-size 4 --code-dim 20');
 $mpdf = new Mpdf();
 $mpdf->img_dpi = 1200;
 
-//Ueberschrift oben
-$html = "<h1>" . $config["header"] . "</h1>";
-
-//Anmelde-Symbol
-$html .= "<img src='oid-" . $product_id . "-START.png' />";
-
-//Stop-Symbol
-$html .= "<div><h2>Stop</h2><img src='oid-" . $product_id . "-stop.png' /></div>";
-
-//Ueber Rows (=Uebungen) gehen
-foreach ($config["rows"] as $row) {
-
-    //Ueberschrift ("Uebung 1")
-    $html .= "<div><h2>" . $row["label"] . "</h2>";
-
-    //Benennung und OID-Code der Uebung als th und td einer Tabelle
-    $th_row = "";
-    $td_row = "";
-
-    //Ueber files (=Geschwindigkeiten) einer Uebung gehen
-    foreach ($row["files"] as $file) {
-
-        //Name (=Tempo 80)
-        $th_row .= "<th>" . $file["name"] . "</th>";
-
-        //OID-Code als Bild
-        $td_row .= "<td><img src='oid-" . $product_id . "-t_" . $row["id"] . "_" . $file["id"] . ".png' /></td>";
-    }
-
-    //fuer diese Uebung einer Tabelle anlegen
-    $html .= "<table><tr>" . $th_row . "</tr><tr>" . $td_row . "</tr></table></div>";
-}
-
 //CSS-Datei laden
 $stylesheet = file_get_contents(__DIR__ . '/styles.css');
 
@@ -116,29 +121,36 @@ $stylesheet = file_get_contents(__DIR__ . '/styles.css');
 $mpdf->WriteHTML($stylesheet, \Mpdf\HTMLParserMode::HEADER_CSS);
 $mpdf->WriteHTML($html, \Mpdf\HTMLParserMode::HTML_BODY);
 
-//Aufraeumen: mp3s fuer scripts entfernen
-foreach (glob("t_*.mp3") as $file) {
-    unlink($file);
-}
-
-//Aufraumen. split mp3s entfernen
-foreach (glob("split_*.mp3") as $file) {
-    unlink($file);
-}
-
-//Aufraeumen: count-in-mp3s entfernen
-foreach (glob("count_*.mp3") as $file) {
-    unlink($file);
-}
-
-//Aufraeumen: oid-pngs entfernen
-foreach (glob("oid-*.png") as $file) {
-    unlink($file);
-}
-
-//Aufrauemen: start / stop.mp3 entfernen
-unlink("start.mp3");
-unlink("stop.mp3");
-
 //pdf als Datei speichern
 $mpdf->Output($project_name . ".pdf");
+
+//Dateisystem aufraeumen
+cleanDir();
+
+//Dateisystem aufraeumen
+function cleanDir() {
+
+    //erzeute split mp3s entfernen
+    foreach (glob("split_*.mp3") as $file) {
+        unlink($file);
+    }
+
+    //count-in-mp3s entfernen
+    foreach (glob("count_*.mp3") as $file) {
+        unlink($file);
+    }
+
+    //generierte mp3s (count-in + Uebung) fuer scripts entfernen
+    foreach (glob("t_*.mp3") as $file) {
+        unlink($file);
+    }
+
+    //oid-pngs entfernen
+    foreach (glob("oid-*.png") as $file) {
+        unlink($file);
+    }
+
+    //start / stop.mp3 entfernen
+    unlink("start.mp3");
+    unlink("stop.mp3");
+}
